e385f9427de60ad6d98b236b7bb843384d756e24
GoLab18 <kuba1golab@gmail.com>
2025-10-16 20:23:26 +0200

    Adjusted treesitter config to the main branch rewrite version

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,51 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  branch = 'main',
-  build = ':TSUpdate',
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "sc",
-        "lua",
-        "vim",
-        "vimdoc",
-        "javascript",
-        "html",
-        "python"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',   
+    build = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter'.setup()
+
+      local langs = {
+        'lua',
+        'vim',
+        'vimdoc',
+        'javascript',
+        'typescript',
+        'html',
+        'css',
+        'python',
+        'json',
+        'jsonc',
+        'go',
+        'gomod',
+        'gosum',
+        'gotmpl',
+        'goctl',
+        'gowork',
+        'java',
+        'javadoc',
+        'c',
+        'cpp',
+        'yaml',
+      }
+
+      require'nvim-treesitter'.install(langs)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = langs,
+        callback = function()
+          -- syntax highlighting, provided by Neovim
+          vim.treesitter.start()
+
+          -- folds, provided by Neovim
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+          -- indentation, provided by nvim-treesitter
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      })
+    end
+  }
 }
