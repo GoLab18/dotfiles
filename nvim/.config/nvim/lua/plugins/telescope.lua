b4f3dfe76c0d202a9fac2d9daead76f4e188fbde
GoLab18 <kuba1golab@gmail.com>
2025-10-16 23:05:11 +0200

    Adjusted telescope branch -> fix the god damn README.md telescope bros COME ON

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    branch = 'master',
     config = function()
         local builtin = require('telescope.builtin')
 
