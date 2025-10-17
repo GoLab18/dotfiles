2c994063e29eea2d0fc9baa77f9c7a026699dd24
GoLab18 <kuba1golab@gmail.com>
2025-10-18 01:17:00 +0200

    Added yank + file expl + wq keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,14 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open file explorer" })
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { desc = "Open neotree filesystem" })
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>q', ':qall<CR>')
+vim.keymap.set('n', '<leader>Q', ':qall!<CR>')
+vim.keymap.set('n', '<leader>wq', ':wqall<CR>')
+
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open file explorer' })
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', { desc = 'Open neotree filesystem' })
