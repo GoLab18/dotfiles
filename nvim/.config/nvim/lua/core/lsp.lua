0b48905bc1d164567846d2c00cfe5b8ff63c20ff
GoLab18 <kuba1golab@gmail.com>
2025-10-18 22:49:10 +0200

    Switched to manual native lsp config from mason and nvim-lspconfig based one

---
+++ nvim/.config/nvim/lua/core/lsp.lua
@@ -0,0 +1,14 @@
+local lsps = {
+  "lua_ls",
+  "gopls",
+}
+
+vim.lsp.enable(lsps)
+
+vim.diagnostic.config {
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+    severity_sort = false,
+}
