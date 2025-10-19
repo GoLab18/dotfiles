97e79cda58425e11b75418c842fbb6a76e1fe246
GoLab18 <kuba1golab@gmail.com>
2025-10-19 18:23:36 +0200

    Added format keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,18 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>w', ':w<CR>')
-vim.keymap.set('n', '<leader>q', ':qall<CR>')
-vim.keymap.set('n', '<leader>Q', ':qall!<CR>')
-vim.keymap.set('n', '<leader>wq', ':wqall<CR>')
+vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":qall<CR>")
+vim.keymap.set("n", "<leader>Q", ":qall!<CR>")
+vim.keymap.set("n", "<leader>wq", ":wqall<CR>")
 
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
+vim.keymap.set({ "n", "v", "x" }, "<leader>d", '"+d<CR>')
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open file explorer' })
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', { desc = 'Open neotree filesystem' })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open file explorer" })
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { desc = "Open neotree filesystem" })
+
+vim.keymap.set("n", "<leader>cf", function()
+	require("conform").format()
+end, { desc = "Formats current file" })
