a181926dfdec7ea19faaff6d349553d449fd4d71
GoLab18 <kuba1golab@gmail.com>
2025-10-19 18:45:55 +0200

    Changed indent type from tabs to spaces

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,56 +1,56 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		branch = "main",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter").setup()
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        branch = "main",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter").setup()
 
-			local langs = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"bash",
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"python",
-				"json",
-				"jsonc",
-				"go",
-				"gomod",
-				"gosum",
-				"gotmpl",
-				"goctl",
-				"gowork",
-				"templ",
-				"java",
-				"javadoc",
-				"c",
-				"cpp",
-				"yaml",
-				"toml",
-				"diff",
-				"markdown",
-			}
+            local langs = {
+                "lua",
+                "vim",
+                "vimdoc",
+                "bash",
+                "javascript",
+                "typescript",
+                "html",
+                "css",
+                "python",
+                "json",
+                "jsonc",
+                "go",
+                "gomod",
+                "gosum",
+                "gotmpl",
+                "goctl",
+                "gowork",
+                "templ",
+                "java",
+                "javadoc",
+                "c",
+                "cpp",
+                "yaml",
+                "toml",
+                "diff",
+                "markdown",
+            }
 
-			require("nvim-treesitter").install(langs)
+            require("nvim-treesitter").install(langs)
 
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = langs,
-				callback = function()
-					-- syntax highlighting, provided by Neovim
-					vim.treesitter.start()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = langs,
+                callback = function()
+                    -- syntax highlighting, provided by Neovim
+                    vim.treesitter.start()
 
-					-- folds, provided by Neovim
-					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    -- folds, provided by Neovim
+                    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
-					-- indentation, provided by nvim-treesitter
-					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-				end,
-			})
-		end,
-	},
+                    -- indentation, provided by nvim-treesitter
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+        end,
+    },
 }
--- nvim/.config/nvim/plugin/ft.lua
+++ nvim/.config/nvim/plugin/ft.lua
@@ -1,10 +1,10 @@
 vim.filetype.add({
-	filename = {
-		[".env"] = "sh",
-		[".envrc"] = "sh",
-	},
-	pattern = {
-		["^%.env.*"] = "sh", -- matches .env, .env.local, etc.
-		["^%.envrc.*"] = "sh", -- matches .envrc, .envrc.local, etc.
-	},
+    filename = {
+        [".env"] = "sh",
+        [".envrc"] = "sh",
+    },
+    pattern = {
+        ["^%.env.*"] = "sh", -- matches .env, .env.local, etc.
+        ["^%.envrc.*"] = "sh", -- matches .envrc, .envrc.local, etc.
+    },
 })
