f1a92ccc8b0440b7a336d4136726eaf1dd84ad78
GoLab18 <kuba1golab@gmail.com>
2025-10-20 13:01:26 +0200

    PHP formatter and linter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -11,6 +11,7 @@ return {
             typescriptreact = { "prettierd", "prettier", stop_after_first = true },
             html = { "prettierd", "prettier", stop_after_first = true },
             css = { "prettierd", "prettier", stop_after_first = true },
+            php = { "php_cs_fixer" },
         },
         format_on_save = {
             timeout_ms = 500,
--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -14,6 +14,7 @@ return {
             typescriptreact = { "eslint_d", "eslint", stop_after_first = true },
             html = { "htmlhint" },
             css = { "stylelint" },
+            php = { "phpstan", "php" },
         }
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
