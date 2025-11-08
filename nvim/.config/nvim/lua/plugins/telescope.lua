312e27010cffa9cb3a119632f441274d5f5cbd88
GoLab18 <kuba1golab@gmail.com>
2025-11-08 15:56:57 +0100

    Added lsp symbols keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -9,5 +9,8 @@ return {
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
         vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+        vim.keymap.set("n", "<leader>o", builtin.lsp_document_symbols, { desc = "Document symbols" })
+        vim.keymap.set("n", "<leader>O", builtin.lsp_dynamic_workspace_symbols, { desc = "Workspace symbols" })
     end,
 }
